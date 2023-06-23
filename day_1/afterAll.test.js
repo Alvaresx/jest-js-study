1ed05309b73515f4c619bf40a1fec4bcbf2bbeea
Mariana Alvares <mariana11areal@hotmail.com>
2023-06-22 21:18:38 -0300

    study(jest): regra console.log no eslint e estudo sobre o afterAll

---
+++ day_1/afterAll.test.js
@@ -0,0 +1,27 @@
+function sum(a, b) {
+  return a + b;
+}
+
+afterAll(() => {
+  console.log("Fui executado após todos os testes!");
+});
+
+describe("Método afterAll", () => {
+  test("deve somar 1 + 2", () => {
+    expect(sum(1, 2)).toBe(3);
+    console.log("Primeiro foi executado aqui.");
+  });
+});
+
+describe("Método afterAll", () => {
+  afterAll(() => {
+    console.log("Fui executado após todos os testes e após a finalização desse bloco describe!");
+  });
+  test("deve somar 2 + 3", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
+
+// Primeiro, o console do primeiro do describe é executado.
+// Logo após, o segundo describe é executado e caso tenha um afterAll dentro dele, ele é executado.
+// Por último é executado o afterAll que estiver do lado de fora de qualquer bloco describe.
